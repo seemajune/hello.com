b19f59eeaef896a7891de060f4a12ee69b5f859d
seemajune <seemashariat@gmail.com>
2015-02-20 11:42:45 -0500

    fixes the nan business on booking conf page yay

--- js/func2.js
+++ js/func2.js
@@ -8,12 +8,24 @@ var total = pricecost * nights;
 var dep = total * .15;
 
 
+
+
 mmcore.AddDocLoadHandler(function() {
     $("#dates").html(from + ' - ' + to);
     $("#ppl").html(guests);
     $("#room").html(rtype);
     $("#cst").html('$' + pricecost);
-    $("#dep").html('$' + dep);
-    $("#tots").html('$' + total);
+    if(!isNaN(dep)) {
+      $("#dep").html('$' + dep);
+    } else {
+      $("#dep").html('$0');
+    }
+    
+    if(!isNaN(total)) {
+      $("#tots").html('$' + total);
+    } else {
+      $("#tots").html('$0');
+    }
+    
     $("#nights").html(nights);
 });
