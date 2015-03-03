5b79edf7c5351001f5b0a12aafb292cd237d7868
seemajune <seemashariat@gmail.com>
2015-03-03 10:54:36 -0500

    fixed error modal button issue now need to post data as visibile in the next step of the funnel

--- js/maxhotel.js
+++ js/maxhotel.js
@@ -1,70 +1,46 @@
 
 $(function() {
-
   $(".booking-form").on('submit', function(e){
     e.preventDefault();
-    
+
     var error = false;
     $("#datefrom, #dateto, #adults").each(function(i,v){
-         if ( $(v).val() == 'Check In' || $(v).val() == 'Check Out' || isNaN(parseInt($(v).val())) ) error = true;
+    // check if user selected dates and # adult guests
+      if ( $(v).val() == 'Check In' || $(v).val() == 'Check Out' || isNaN(parseInt($(v).val())) ) error = true;
     });
-    if (error) return $('.error-modal').addClass('open-modal');
+
+    // get total nights when dates are valid
+    var checkInDateAsDateObj = new Date($('#datefrom').val());
+    var checkOutDateAsDateObj = new Date($('#dateto').val());
+    var today = new Date();
+    var nights = Math.floor((checkOutDateAsDateObj - checkInDateAsDateObj)/(1000*60*60*24));
+    //check if dates are valid
+    if( today > checkInDateAsDateObj || checkOutDateAsDateObj < checkInDateAsDateObj ) error = true;
+
+    // show error modal if any above cases result in true error(s)
+    if (error) {
+      $('.error-modal').addClass('open-modal');
     
+      // close the modal, re-enable booking button
+      $('.error-button').on('click', function() {
+        $('.error-modal').removeClass('open-modal');
+       });
+
+      return;
+    }
+
+    // on success, redirect to booking page, post data
     $.ajax({  
       type: $('.booking-form').attr('method'),  
       url: $('.booking-form').attr('action'),  
       data: $('.booking-form').serialize(),       
       success: function(){  
-        alert("success"); 
-        window.location.href = '/booking3.html';
+        window.location.href = '/booking2.html';
+        alert();
+          // $('#dates').text(results.book_date_from.data());
       }  
     });
-  });
-
-  // close the modal, re-enable book button
-  $('.error-button').on('click', function() {
-    $('.error-modal').removeClass('open-modal');
   });
 });
 
 
-// $(function() {
-//   $('.bookbutton').on('click', function(e){
-//       var checkin = $('#datefrom').val();
-//       // gets checkin date as string
-//       var checkout = $('#dateto').val();
-//       // gets checkout date as string
-//       var adults = parseInt($('#adults').val());
-//       // gets total number of adult guests as numeric value
-
-//       if(checkin ==  "Check In" || checkout == "Check Out"){
-//         //alert the user that they cannot proceed/submit form without filling out checkin or checkout dates. 
-//         e.preventDefault();
-//         alert("you must select valid checkin and checkout dates");
-//       } 
-//       if(isNaN(adults)){
-//         // alert the user that they cannot proceed/submit form without filling # adults. 
-//         e.preventDefault();
-//        alert("please select # of adult guests");
-//       }
-//       //formulate date obj out of checkin and checkout strings:
-//       var checkInDateAsDateObject = new Date(checkin);
-//       var checkOutDateAsDateObject = new Date(checkout);
-//       // get difference in numeric value (total nights)
-
-//       var today = new Date();
-//       //ensure the checkin date is before the checkout date
-
-//       //ensure the checkin date is in the future as compared to current date
-//       if(today > checkInDateAsDateObject || checkOutDateAsDateObject < checkInDateAsDateObject){
-//         e.preventDefault();
-//       alert("please select a valid checkin/checkout date");
-//       }
-//       else {
-//         var nights = Math.floor((checkOutDateAsDateObject - checkInDateAsDateObject)/(1000*60*60*24));
-//       }
-      
-//   });
-
-// });
- 
